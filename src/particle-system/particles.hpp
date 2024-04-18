071d55d44a15fe328a32b73a0460a853d02ca584
lunncurt <lunncurt@msu.edu>
2024-04-17 22:37:10 -0400

    initial sfml particle class header

--- src/particle-system/particles.hpp
+++ src/particle-system/particles.hpp
@@ -1,21 +1,27 @@
 #pragma once
 
-#include </opt/homebrew/Cellar/glm/1.0.1/include/glm/glm.hpp>
+#include "SFML/Graphics/Glsl.hpp"
+#include <SFML/Graphics.hpp>
 
 class Particle {
 public:
-  Particle(const glm::vec2 &position, const glm::vec2 &velocity, float mass);
+  Particle(const sf::CircleShape part, sf::Glsl::Vec2 position,
+           sf::Glsl::Vec2 velocity, float mass);
 
-  glm::vec2 get_position() const { return position; }
-  glm::vec2 get_velocity() const { return velocity; }
-  float get_mass() const { return mass; }
+  sf::Glsl::Vec2 get_position() const { return position; };
+  sf::Glsl::Vec2 get_velocity() const { return velocity; };
+  float get_mass() const { return mass; };
 
-  void setPosition(const glm::vec2 &newPosition) { position = newPosition; }
-  void setVelocity(const glm::vec2 &newVelocity) { velocity = newVelocity; }
-  void setMass(float newMass) { mass = newMass; }
+  void set_position(const sf::Glsl::Vec2 &newPosition) {
+    position = newPosition;
+  };
+  void set_velocity(const sf::Glsl::Vec2 &newVelocity) {
+    velocity = newVelocity;
+  };
+  void set_mass(float newMass) { mass = newMass; };
 
 private:
-  glm::vec2 position;
-  glm::vec2 velocity;
+  sf::Glsl::Vec2 position;
+  sf::Glsl::Vec2 velocity;
   float mass;
 };
